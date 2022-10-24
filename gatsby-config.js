b76036dabc2266cdb666defee41162c301434f23
SahanNimeshWeerakoon <hsn.weerakoon.m@gmail.com>
2022-10-24 19:32:25 +0530

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Gatsby Crash Course`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    keywords: 'react, gatsby, tutorial'
   },
   plugins: [
     `gatsby-plugin-image`,
